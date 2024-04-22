3dcb8c445a7dc087aaeba2b516d1ed71729c1a30
sofiacottone <cottonesofia@outlook.it>
2024-04-22 22:04:58 +0200

    feat: import match section component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script>
 import PageHeader from './components/PageHeader.vue';
+import MatchSection from './components/MatchSection.vue';
 
 export default {
   components: {
-    PageHeader
+    PageHeader,
+    MatchSection
   }
 }
 </script>
 
 <template>
+
   <PageHeader></PageHeader>
+
+  <main>
+    <MatchSection></MatchSection>
+  </main>
+
 </template>
 
 <style lang="scss">
