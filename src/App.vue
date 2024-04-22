566f53a787befa9e22de4ec4e3d93e66471efe12
sofiacottone <cottonesofia@outlook.it>
2024-04-22 14:53:29 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script></script>
+
+<template>
+  <div class="container">
+    <div class="card">ciao</div>
+  </div>
+</template>
+
+<style lang="scss">
+@use './style/generic.scss' as *;
+</style>
