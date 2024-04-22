8eee92fcf73fb8f4c7e25b4bb219b75b683b23a4
sofiacottone <cottonesofia@outlook.it>
2024-04-22 22:57:26 +0200

    feat: add upcoming matches component + import single match component

---
+++ src/components/UpcomingSection.vue
@@ -0,0 +1,35 @@
+<script>
+import SingleMatch from '../components/SingleMatch.vue'
+export default {
+    name: 'UpcomingSection',
+    components: {
+        SingleMatch
+    }
+}
+</script>
+
+<template>
+    <div class="container-fluid text-white py-4">
+        <h3 class="pb-4 text-center">Upcoming Matches</h3>
+
+        <SingleMatch></SingleMatch>
+        <SingleMatch></SingleMatch>
+        <SingleMatch></SingleMatch>
+        <SingleMatch></SingleMatch>
+        <SingleMatch></SingleMatch>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+.container-fluid {
+    background-image: url(../assets/img/saha.jpg);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    min-height: 600px;
+
+
+}
+</style>
