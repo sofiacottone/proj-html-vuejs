5cdfa66d3f0a27f888aea4d38cb13de80823a286
sofiacottone <cottonesofia@outlook.it>
2024-04-22 22:05:36 +0200

    feat: add hero structure

---
+++ src/components/PageHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'PageHero'
+}
+</script>
+
+<template>
+    <div class="ms-hero d-flex justify-content-between align-items-center">
+        <div>left</div>
+        <div>
+            <h1 class="fw-bold">Football Club Sport Club</h1>
+        </div>
+        <div>right</div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+.ms-hero {
+    height: calc($full-header-height - $top-header-height);
+
+    h1 {
+        font-size: 60px;
+    }
+}
+</style>
