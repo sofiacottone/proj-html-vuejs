4511bd304fde22b69f63f874f56ef12e8d654403
sofiacottone <cottonesofia@outlook.it>
2024-04-23 12:28:01 +0200

    feat: add clients section

---
+++ src/components/ClientsSection.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'ClientsSection',
+}
+</script>
+
+<template>
+    <div class="container-fluid text-white p-4">
+        <div class="d-flex justify-content-center align-items-center gap-4 my-4">
+            <div class="border rounded-5 text-center px-2">
+                <img class="img-fluid" src="../assets/img/client2.png" alt="">
+            </div>
+            <div class="border rounded-5 text-center px-2">
+                <img class="img-fluid" src="../assets/img/client3.png" alt="">
+            </div>
+            <div class="border rounded-5 text-center px-2">
+                <img class="img-fluid" src="../assets/img/client4.png" alt="">
+            </div>
+            <div class="border rounded-5 text-center px-2">
+                <img class="img-fluid" src="../assets/img/client7.png" alt="">
+            </div>
+            <div class="border rounded-5 text-center px-2">
+                <img class="img-fluid" src="../assets/img/client9.png" alt="">
+            </div>
+            <div class="border rounded-5 text-center px-2">
+                <img class="img-fluid" src="../assets/img/client10.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+.container-fluid {
+    min-height: 260px;
+    background-color: #000;
+
+}
+</style>
