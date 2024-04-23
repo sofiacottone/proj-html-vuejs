bd5b2c23fa848a84311658cb136da4334c2b918f
sofiacottone <cottonesofia@outlook.it>
2024-04-23 11:57:04 +0200

    feat: import tables component

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import PageHeader from './components/PageHeader.vue';
 import MatchSection from './components/MatchSection.vue';
 import UpcomingSection from './components/UpcomingSection.vue';
 import PlayersSection from './components/PlayersSection.vue';
+import TablesSection from './components/TablesSection.vue';
 
 export default {
   components: {
     PageHeader,
     MatchSection,
     UpcomingSection,
-    PlayersSection
+    PlayersSection,
+    TablesSection
   }
 }
 </script>
@@ -22,6 +24,7 @@ export default {
     <MatchSection></MatchSection>
     <UpcomingSection></UpcomingSection>
     <PlayersSection></PlayersSection>
+    <TablesSection></TablesSection>
   </main>
 
 </template>
