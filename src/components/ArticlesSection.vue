3d37584b0a2e8b5a1c0024c21a4d951b95a6f6bc
sofiacottone <cottonesofia@outlook.it>
2024-04-24 19:42:38 +0200

    feat: add function to activate article on click

--- src/components/ArticlesSection.vue
+++ src/components/ArticlesSection.vue
@@ -1,64 +1,48 @@
 <script>
+import { store } from '../store.js'
+
 export default {
     name: 'ArticlesSection',
+    data() {
+        return {
+            store,
+            activeArticle: 0
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../assets/img/${name}`, import.meta.url).href;
+        },
+        activateArticle(clickedIndex) {
+            this.activeArticle = clickedIndex;
+        },
+        horizontalScroll(event) {
+            event.preventDefault();
+            event.currentTarget.scrollLeft += event.deltaY
+        },
+    }
 
 }
 </script>
 
 <template>
-    <div class="container-fluid p-4">
+    <div id="articles" class="container-fluid p-4">
         <div class="ms-thumbs-up text-center my-3">
             <img src="../assets/img/thumbs-up.png" class="rounded-circle overflow-hidden p-3">
         </div>
         <h2 class="pb-4 fw-bold fs-2 text-center">Resent New & Articles</h2>
 
         <!-- cards  -->
-        <div class="d-flex justify-content-center gap-3">
-            <div class="card col-3 border-0">
-                <img src="../assets/img/news1-1.png" class="card-img-top p-4 pb-1" alt="...">
-                <div class="text-center">29.05.2022 - Football</div>
+        <div @wheel="horizontalScroll($event)"
+            class="d-flex justify-content-start gap-3 overflow-y-auto ms-no-scrollbar p-3 pb-5">
+            <div v-for="(article, index) in store.articlesInfo" @click="activateArticle(index)"
+                :class="{ 'active': index == activeArticle }" class="card col-3 border-0">
+                <img :src="getImageUrl(article.img)" class="card-img-top p-4 pb-1" alt="...">
+                <div class="text-center">{{ article.date }} - {{ article.topic }}</div>
                 <div class="card-body">
-                    <h5 class="card-title text-center fw-bold pb-2">What is Football?</h5>
-                    <p class="card-text">Football is a sport that is known as a ball game and il loved by [...]
-                    </p>
+                    <h5 class="card-title text-center fw-bold pb-2">{{ article.title }}</h5>
+                    <p class="card-text">{{ article.paragraph }}</p>
                     <div class="d-flex justify-content-center mb-3">
-                        <a href="#" class="btn btn-dark rounded-5 px-5 py-3">More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card col-3 border-0">
-                <img src="../assets/img/news3-1.png" class="card-img-top p-4 pb-1" alt="...">
-                <div class="text-center">29.05.2022 - Football</div>
-                <div class="card-body">
-                    <h5 class="card-title text-center fw-bold pb-2">Football Rules</h5>
-                    <p class="card-text">GOLDEN GOAL: In a football match, the goal scored by one of the teams in [...]
-                    </p>
-                    <div class="d-flex justify-content-center mb-3">
-                        <a href="#" class="btn btn-dark rounded-5 px-5 py-3">More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card col-3 border-0">
-                <img src="../assets/img/news2-1.png" class="card-img-top p-4 pb-1" alt="...">
-                <div class="text-center">29.05.2022 - Football</div>
-                <div class="card-body">
-                    <h5 class="card-title text-center fw-bold pb-2">Football Features</h5>
-                    <p class="card-text">Football: it is an aerobic-based anaerobic sport in which jumps, kicks, turns,
-                        running with changing [...]
-                    </p>
-                    <div class="d-flex justify-content-center">
-                        <a href="#" class="btn btn-dark rounded-5 px-5 py-3">More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card col-3 border-0">
-                <img src="../assets/img/news4-1.png" class="card-img-top p-4 pb-1" alt="...">
-                <div class="text-center">29.05.2022 - Football</div>
-                <div class="card-body">
-                    <h5 class="card-title text-center fw-bold pb-2">Football Terms</h5>
-                    <p class="card-text">GOLDEN GOAL: In a football match, the goal scored by one of the teams in [...]
-                    </p>
-                    <div class="d-flex justify-content-center">
                         <a href="#" class="btn btn-dark rounded-5 px-5 py-3">More</a>
                     </div>
                 </div>
@@ -66,11 +50,9 @@ export default {
         </div>
 
         <!-- slider  -->
-        <div class="d-flex justify-content-center gap-1 mt-5">
-            <div class="ms-slider rounded-pill"></div>
-            <div class="ms-slider rounded-pill"></div>
-            <div class="ms-slider active rounded-pill"></div>
-            <div class="ms-slider rounded-pill"></div>
+        <div class="d-flex justify-content-center gap-1 mt-3">
+            <div v-for="(n, index) in store.articlesInfo.length" @click="activateArticle(index)"
+                class="ms-slider rounded-pill" :class="{ 'active': index == activeArticle }"></div>
         </div>
     </div>
 </template>
@@ -84,18 +66,23 @@ export default {
     .ms-thumbs-up {
         img {
             background-color: #000;
-
         }
     }
 
     .card {
         box-shadow: 2px 2px 20px #00000050;
         max-width: 300px;
+        height: fit-content;
+
+        &.active {
+            transform: scale(1.1);
+            margin-inline: 16px;
+        }
     }
 
     .ms-slider {
         height: 6px;
-        width: 60px;
+        width: 50px;
         background: $dark-gray;
 
         &.active {
@@ -104,5 +91,14 @@ export default {
         }
     }
 
+}
+
+.ms-no-scrollbar {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
 }
 </style>
