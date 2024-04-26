7c91634b70edaec7c4424f4d6dd8cb51507b2200
sofiacottone <cottonesofia@outlook.it>
2024-04-26 12:36:54 +0200

    feat: add function to show more teams on click

--- src/components/TablesSection.vue
+++ src/components/TablesSection.vue
@@ -1,6 +1,28 @@
 <script>
+import { store } from '../store.js'
+
 export default {
     name: 'TablesSection',
+    data() {
+        return {
+            store,
+            showLessTeams: true,
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../assets/img/${name}`, import.meta.url).href;
+        },
+    },
+    computed: {
+        teamsToDisplay() {
+            if (this.showLessTeams) {
+                return this.store.teamsInfo.slice(0, 5);
+            } else {
+                return this.store.teamsInfo;
+            }
+        },
+    },
 }
 </script>
 
@@ -9,7 +31,7 @@ export default {
         <!-- tables  -->
         <div class="d-flex justify-content-center gap-5">
             <!-- league table  -->
-            <table class="caption-top border col-5">
+            <table id="league" class="caption-top border col-5">
                 <caption class="text-center h4 fw-bold">League Table</caption>
                 <thead class="text-uppercase">
                     <th class="p-1 ps-2" colspan="2">team</th>
@@ -19,145 +41,28 @@ export default {
                     <th class="ps-1 pe-0 text-center">pts</th>
                 </thead>
                 <tbody>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start text-start" scope="row">1</th>
+                    <tr v-for="team in teamsToDisplay" class="text-center">
+                        <th class="py-2 ps-2 text-start text-start" scope="row">{{ team.id }}</th>
                         <td class="d-flex align-items-center">
                             <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/club-1.png" alt="">
+                                <img class="img-fluid" :src="getImageUrl(team.logo)" :alt="team.name">
                             </div>
                             <span>
-                                Istanbul Sports
+                                {{ team.name }}
                             </span>
                         </td>
-                        <td>18</td>
-                        <td>2</td>
-                        <td>0</td>
-                        <td>42</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">2</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/club-2.png" alt="">
-                            </div>
-                            <span>
-                                United Fs Club
-                            </span>
-                        </td>
-                        <td>14</td>
-                        <td>2</td>
-                        <td>1</td>
-                        <td>35</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">3</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/club-3.png" alt="">
-                            </div>
-                            <span>
-                                Schalke Club
-                            </span>
-                        </td>
-                        <td>13</td>
-                        <td>3</td>
-                        <td>0</td>
-                        <td>32</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">4</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/club-4.png" alt="">
-                            </div>
-                            <span>
-                                Inter Club
-                            </span>
-                        </td>
-                        <td>13</td>
-                        <td>2</td>
-                        <td>1</td>
-                        <td>31</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">5</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/league-club-5.png" alt="">
-                            </div>
-                            <span>
-                                Madrid Club
-                            </span>
-                        </td>
-                        <td>12</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>30</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">6</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/league-club-6.png" alt="">
-                            </div>
-                            <span>
-                                Schalke Club
-                            </span>
-                        </td>
-                        <td>11</td>
-                        <td>0</td>
-                        <td>3</td>
-                        <td>28</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">7</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/league-club-7.png" alt="">
-                            </div>
-                            <span>
-                                Arsenal Club
-                            </span>
-                        </td>
-                        <td>10</td>
-                        <td>6</td>
-                        <td>0</td>
-                        <td>26</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">8</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/league-club-8.png" alt="">
-                            </div>
-                            <span>
-                                Bayern Club
-                            </span>
-                        </td>
-                        <td>10</td>
-                        <td>2</td>
-                        <td>2</td>
-                        <td>24</td>
-                    </tr>
-                    <tr class="text-center">
-                        <th class="py-2 ps-2 text-start" scope="row">9</th>
-                        <td class="d-flex align-items-center">
-                            <div class="ms-club-logo me-2 my-1">
-                                <img class="img-fluid" src="../assets/img/league-club-9.png" alt="">
-                            </div>
-                            <span>
-                                Atletico Club
-                            </span>
-                        </td>
-                        <td>9</td>
-                        <td>0</td>
-                        <td>4</td>
-                        <td>20</td>
+                        <td>{{ team.win }}</td>
+                        <td>{{ team.draw }}</td>
+                        <td>{{ team.lose }}</td>
+                        <td>{{ team.points }}</td>
                     </tr>
                 </tbody>
                 <tfoot>
                     <tr class="text-center fw-bold">
-                        <td class="py-3" colspan="6">View Full League Table</td>
+                        <td @click="showLessTeams = !showLessTeams" class="py-3" colspan="6">
+                            {{ showLessTeams === true ? "View Full League Table" : "Show Less" }}
+
+                        </td>
                     </tr>
                 </tfoot>
             </table>
@@ -358,6 +263,7 @@ export default {
 
     table {
         max-width: 600px;
+        height: fit-content;
         border-collapse: collapse;
         border-style: hidden;
 
@@ -378,6 +284,8 @@ export default {
     }
 
     #fixtures {
+        height: auto;
+
         tbody {
             tr:nth-child(even) {
                 background-color: $dark-gray;
