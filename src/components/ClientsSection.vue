2e9261f03e079c2540e0070853ea3c7d51cf8a35
sofiacottone <cottonesofia@outlook.it>
2024-04-26 12:38:38 +0200

    feat: add images dynamically

--- src/components/ClientsSection.vue
+++ src/components/ClientsSection.vue
@@ -1,29 +1,31 @@
 <script>
 export default {
     name: 'ClientsSection',
+    data() {
+        return {
+            clients: [
+                'client2.png',
+                'client3.png',
+                'client4.png',
+                'client7.png',
+                'client9.png',
+                'client10.png',
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../assets/img/${name}`, import.meta.url).href;
+        }
+    }
 }
 </script>
 
 <template>
     <div class="container-fluid text-white p-4">
         <div class="d-flex justify-content-center align-items-center gap-4 my-4">
-            <div class="border rounded-5 text-center px-2">
-                <img class="img-fluid" src="../assets/img/client2.png" alt="">
-            </div>
-            <div class="border rounded-5 text-center px-2">
-                <img class="img-fluid" src="../assets/img/client3.png" alt="">
-            </div>
-            <div class="border rounded-5 text-center px-2">
-                <img class="img-fluid" src="../assets/img/client4.png" alt="">
-            </div>
-            <div class="border rounded-5 text-center px-2">
-                <img class="img-fluid" src="../assets/img/client7.png" alt="">
-            </div>
-            <div class="border rounded-5 text-center px-2">
-                <img class="img-fluid" src="../assets/img/client9.png" alt="">
-            </div>
-            <div class="border rounded-5 text-center px-2">
-                <img class="img-fluid" src="../assets/img/client10.png" alt="">
+            <div v-for="client in clients" class="border rounded-5 text-center px-2">
+                <img class="img-fluid" :src="getImageUrl(client)" alt="">
             </div>
         </div>
     </div>
@@ -33,7 +35,7 @@ export default {
 @use '../style/partials/variables' as *;
 
 .container-fluid {
-    min-height: 260px;
+    min-height: 220px;
     background-color: #000;
 
 }
