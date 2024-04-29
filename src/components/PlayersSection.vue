040ca5bb878596705d24fa854c01bcc800c3ae51
sofiacottone <cottonesofia@outlook.it>
2024-04-29 09:53:04 +0200

    feat: add field with team formation

--- src/components/PlayersSection.vue
+++ src/components/PlayersSection.vue
@@ -7,6 +7,7 @@ export default {
         return {
             store,
             showLessPlayers: true,
+            isFieldVisible: false
         }
     },
     methods: {
@@ -31,7 +32,7 @@ export default {
         <h3 class="pb-4 text-center">Players</h3>
 
         <!-- player cards  -->
-        <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="row row-cols-1 row-cols-md-4 g-4" :class="[!isFieldVisible ? 'd-flex' : 'd-none']">
             <div v-for="player in playersToDisplay" class="col ms-single-card">
                 <div class="border-0 rounded-5 rounded-bottom-4 overflow-hidden position-relative">
                     <img :src="getImageUrl(player.img)" class="card-img-top" :alt="player.name">
@@ -43,11 +44,58 @@ export default {
             </div>
         </div>
 
+        <!-- field  -->
+        <div class="d-flex justify-content-around my-3">
+            <div :class="[isFieldVisible ? 'd-block' : 'd-none']">
+                <div class="bg-dark rounded-pill p-2 fw-bold text-center">Name (number) - Role</div>
+                <ol class="mt-2">
+                    <template v-for="player in store.playersInfo">
+                        <li class="px-1 py-1"><span class="fw-bold">{{ player.name }}</span> ({{ player.number }}) - {{
+            player.role }}
+                        </li>
+                    </template>
+                </ol>
+            </div>
+            <div>
+                <div :class="[isFieldVisible ? 'd-block' : 'd-none']" class="text-center font-monospace p-2">3-4-3</div>
+                <div id="field" :class="[isFieldVisible ? 'd-flex' : 'd-none']"
+                    class="flex-column align-items-center gap-5 px-3 py-4">
+                    <!-- Goalkeeper -->
+                    <div class="ms-player gk rounded-circle overflow-hidden border bg-dark">GK</div>
+
+                    <!-- Defenders -->
+                    <div class="d-flex mb-3 mt-2">
+                        <div class="ms-player defender rounded-circle overflow-hidden border bg-dark me-5 mt-4">DEF
+                        </div>
+                        <div class="ms-player defender rounded-circle overflow-hidden border bg-dark me-5">DEF</div>
+                        <div class="ms-player defender rounded-circle overflow-hidden border bg-dark mt-4">DEF</div>
+                    </div>
+                    <!-- Midfielders -->
+                    <div class="d-flex gap-4">
+                        <div class="ms-player midfielder rounded-circle overflow-hidden border bg-dark">MID</div>
+                        <div class="ms-player midfielder rounded-circle overflow-hidden border bg-dark">MID</div>
+                        <div class="ms-player midfielder rounded-circle overflow-hidden border bg-dark">MID</div>
+                        <div class="ms-player midfielder rounded-circle overflow-hidden border bg-dark">MID</div>
+                    </div>
+                    <!-- Forwards -->
+                    <div class="d-flex gap-5 mt-4">
+                        <div class="ms-player forward rounded-circle overflow-hidden border bg-dark mt-3">FWD</div>
+                        <div class="ms-player forward rounded-circle overflow-hidden border bg-dark">FWD</div>
+                        <div class="ms-player forward rounded-circle overflow-hidden border bg-dark mt-3">FWD</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <!-- button  -->
         <div class="d-flex justify-content-center align-items-center gap-3 mt-5">
             <button class="btn btn-outline-light rounded-5 py-4 px-5" @click="showLessPlayers = !showLessPlayers">
                 {{ showLessPlayers === true ? "View All Players" : "Show Less" }}
             </button>
+            <div @click="isFieldVisible = !isFieldVisible; showLessPlayers = true"
+                class="ms-field btn rounded-5 py-2 px-4">
+                <img src="../assets/img/football-field-sm.png">
+            </div>
         </div>
     </div>
 </template>
@@ -92,6 +140,10 @@ export default {
         }
     }
 
+    ol {
+        backdrop-filter: blur(16px) saturate(180%);
+    }
+
     #field {
         background-image: url(../assets/img/football_field.png);
         background-size: cover;
@@ -103,6 +155,9 @@ export default {
         .ms-player {
             width: 60px;
             height: 60px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
         }
     }
 }
